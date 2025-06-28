e919a053a9e431af0dcd396c6e30ada8d9662f07
feroddev <neath.felipe@gmail.com>
2025-06-28 20:48:48 -0300

    feat: implementa serviço de telemetria com funções para salvar dados de giroscópio, GPS e fotos

---
+++ internal/services/telemetry_service.go
@@ -0,0 +1,34 @@
+package services
+
+import (
+	"github/feroddev/challengeV3/internal/core"
+	"github/feroddev/challengeV3/internal/repositories"
+)
+
+type TelemetryService interface {
+	SaveGyroscopeData(data core.Gyroscope) error
+	SaveGPSData(data core.GPS) error
+	SavePhotoData(data core.Photo) error
+}
+
+type telemetryService struct {
+	repository repositories.TelemetryRepository
+}
+
+func NewTelemetryService(repository repositories.TelemetryRepository) TelemetryService {
+	return &telemetryService{
+		repository: repository,
+	}
+}
+
+func (s *telemetryService) SaveGyroscopeData(data core.Gyroscope) error {
+	return s.repository.SaveGyroscopeData(data)
+}
+
+func (s *telemetryService) SaveGPSData(data core.GPS) error {
+	return s.repository.SaveGPSData(data)
+}
+
+func (s *telemetryService) SavePhotoData(data core.Photo) error {
+	return s.repository.SavePhotoData(data)
+}
